1d61518ed2aa100cc6ebf8bd9bac4429887962df
John Griffiths <gnuchu@gmail.com>
2017-10-19 16:48:15 +0100

    Initial Commit

---
+++ NewPWGen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewPWGen
+//
+//  Created by John Griffiths on 19/10/2017.
+//  Copyright © 2017 John Griffiths. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
